14992e28f6c650d2841a63fa8af004537f9b1885
Doopelganger <n9rvod@gmail.com>
2017-01-31 11:33:03 +0100

    Add files via upload

--- ChessDataMining/basic.py
+++ ChessDataMining/basic.py
@@ -3,14 +3,22 @@
 Created on Tue Dec 27 12:24:17 2016
 
 @author: 21003607
+
+Objectif : 
+    - parser un fichier PGN pour extraire des informations sur des parties d'echec
+    - construire une table de traits a partir de ces informations pour caracteriser des classes de joueurs
+    - tester ces donnees sur un moteur d'apprentissage
+    
 """
 import chess
 import chess.pgn # parsing du fichier pgn
 import chess.uci # moteur de jeu
 
 # donnes de jeu -- fichier pgn
-pgn = open("data/pgn/test.pgn")
+pgn = open("data/pgn/testElo.pgn")
 
+# fichier de sortie avec les tables
+output = open("data/output/result", "w")
 
 # moteur  Oracle
 engine = chess.uci.popen_engine("stockfish/Linux/stockfish_8_x64")
@@ -19,9 +27,7 @@ engine.uci()
 info_handler = chess.uci.InfoHandler()
 engine.info_handlers.append(info_handler)
 
-
-# fichier output
-
+# flag pour la duree de vie du traitement
 execute = True
 
 while(execute):
@@ -36,6 +42,7 @@ while(execute):
     
         """
         Recuperation des joueurs
+        Initialisation des variables pour classifier la partie
         """
         
         white = current.headers["White"]
@@ -47,7 +54,9 @@ while(execute):
         whiteElo = current.headers["WhiteElo"]
         blackElo = current.headers["BlackElo"]        
         
-        # determination de la classe de chaque joueur
+        # determination de la classe de chaque joueur -- necessite que le ELO des joueurs soient indiques dans le PGN
+        classWhite = "0"
+        classBlack = "0"
         if(whiteElo and blackElo != ''):
             if(int(whiteElo) > 2400):
                 classWhite = 1
@@ -55,34 +64,55 @@ while(execute):
             else:
                 classWhite = 0
                 
-            
             if(int(blackElo) > 2400):
                 classBlack = 1
                 
             else:
-                classBlack = 0            
+                classBlack = 0 
+        else:
+            # statement pour passer automatiquement à la prochaine entree de partie si les ELO ne sont pas indiques
+            continue
         
-        # init des attributs de la partie            
-        nbcoup = 0              # nombre de coup
+        #init des attributs de la partie     
         
-        scoreW = 0              # score pour white
-        scoreB = 0              # score pour black
-        illegal_movesW = 0      # nombre de coups illegaux pour white
-        illegal_movesB = 0      # nombre de coups illegaux pour black
+        # nombre de coup
+        nbcoup = 0              
         
-        white_serie = 0         # plus longue serie de coups proches de l'Oracle pour white
-        black_serie = 0         # plus longue serie de coups proches de l'Oracle pour black
+        # score Oracle -- represente en moyenne pour la partie le ratio de coup Joueur proches des coups Oracle
+        scoreW = 0              
+        scoreB = 0
         
-        scoreGame = 0           # score cp pour la partie
-        # init du board pour l'Oracle
-        board = chess.Board();        
+        # nombre de coups illegaux pour white
+        illegal_movesW = 0      
+        illegal_movesB = 0
         
+        # plus longue serie de coups proches de l'Oracle
+        white_serie = 0
+        maxW = 0         
+        black_serie = 0         
+        maxB = 0
+        # moyenne du score cp pour la partie
+        scoreGame = 0           
+        
+        # coup servant d'ouverture pour la partie
+        eco = current.headers["ECO"]
+        
+        # premiere piece superieure 
+        firstW = ""
+        firstB = ""
+        
+   
         """
         Parsing de la partie
         """
+        # init du board pour l'Oracle
+        board = chess.Board();        
         
         moves = current.main_line()
         node = current
+        
+        colors = ["W", "B"]
+        i = 0
         
         while not node.is_end():
             
@@ -94,16 +124,27 @@ while(execute):
                 puis demander au moteur le coup correspondant
             """
             # incrementation du nombre de coup -- a modifier
-            nbcoup += nbcoup
-            
+            nbcoup += 1
+            player = colors[i]
+            print(nbcoup)
             # node
             next_node = node.variation(0)
+            pgn_move = node.board().san(next_node.move)
             print(node.board().san(next_node.move))
             
             # coup joue
             move = chess.Move.uci(next_node.move)
             print(move)
             
+            # recuperation de la premiere piece sup jouee
+            pieces = 'KQRBN'
+            for p in pgn_move :
+                if(p in pieces):
+                    if(player == "W" and firstW ==""):
+                        firstW = p
+                    elif(firstB == ""):
+                        firstB = p
+                        
             # coups legaux du board courant
             legalmoves = board.legal_moves
             # test la legalite du coup -- support pour la classification
@@ -119,15 +160,23 @@ while(execute):
             # donner l'etat courant du board a l'Oracle
             engine.position(board)
             # recherche du meilleur coup a jouer selon l'Oracle
-            oracle_move = engine.go(movetime = 2000)            
-            print(oracle_move.bestmove)
-
-            if(move == oracle_move.bestmove):
-                print(1)
+            oracle_move = engine.go()   # movetime = 2000 -- init la recherche pour 2000 milliseconds -- non interessant pour l'ampleur du fichier -- 20K+ lignes
+            best_move = oracle_move.bestmove
+            if(str(move) == str(best_move)):
+                print("best")
                 if(player == "W"):
                     white_serie += 1
+                    scoreB += 1
                 else:
                     black_serie += 1
+                    scoreW += 1
+            else:
+                if(player == "W"):
+                    maxW = white_serie
+                    white_serie = 0
+                else:
+                    maxB = black_serie
+                    black_serie = 0
                 
             
             # une fois toutes les operations executees
@@ -139,11 +188,30 @@ while(execute):
             current_score = info_handler.info["score"][1].cp
             print(current_score)
             
-            
             print("")
-            break
+            i = (i+1)%2
+            node = next_node
         
-            
+        
+        """
+        calcul des donnees
+        """        
+        scoreW =  float("{0:.2f}".format(scoreW / nbcoup))
+        scoreB = float("{0:.2f}".format(scoreB / nbcoup))
+        
+        """
+        ecriture des resultats
+        structure : Classe ; moyenneOracle ; pluslongueserie ; nombrecoups ; ouverture ; premierepiece ; 
+        """
+        lineW = str(classWhite) + ";" + str(scoreW) + ";" + str(max(white_serie, maxW)) + ";" + str(nbcoup) + ";" + str(eco) + ";" + str(firstW) + "\n"
+        lineB = str(classBlack) + ";" + str(scoreB) + ";" + str(max(black_serie, maxB)) + ";" + str(nbcoup) + ";" + str(eco) + ";" + str(firstB) + "\n"
+        
+        output.write(lineW)
+        output.write(lineB)
+        
+output.close()
+engine.quit()
+        
         
             
 
